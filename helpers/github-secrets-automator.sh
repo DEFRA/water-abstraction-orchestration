b1b83d0bcd6b85b6d50fe05e7bee52546638031a
Tim <tim@tamimi.dev>
2022-03-10 11:54:30 +0000

    Update process to flow through repos in a loop

--- helpers/github-secrets-automator.sh
+++ helpers/github-secrets-automator.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. ../shared/repos.sh
 
 cd "$(dirname "$0")" || exit
 
@@ -9,27 +10,15 @@ rootDir=${rootDir:-"../../"}
 echo "Using $rootDir as root path"
 echo "Taking secrets from .env files and adding them to Github repo Action Secrets"
 
+
+# For each WRLS repo, load the shared secrets
+for repository in "${repositories[@]}"
+do
+    echo "#################### $repository ####################"
+    cd $rootDir+$repository
+    gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water"
+done
+
+# Additionally, the water service repo needs the charge module secrets!
 cd $rootDir+"water-abstraction-service"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-api"
 gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.charge"
-
-cd $rootDir+"water-abstraction-ui"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-ui"
-
-cd $rootDir+"water-abstraction-returns"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-returns"
-
-cd $rootDir+"water-abstraction-permit-repository"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-permit-repo"
-
-cd $rootDir+"water-abstraction-import"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-import"
-
-cd $rootDir+"water-abstraction-reporting"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-reporting"
-
-cd $rootDir+"water-abstraction-tactical-crm"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-crm"
-
-cd $rootDir+"water-abstraction-tactical-idm"
-gh secret set test_scret --env-file $rootDir"water-abstraction-orchestration/secrets/.env.water-idm"
