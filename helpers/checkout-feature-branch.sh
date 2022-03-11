e12963334df205db31bb09ba79681c0dcc790422
Jonathan Goulding <jonathangoulding21@gmail.com>
2022-03-11 08:23:20 +0000

    feat: checkout feature branch or default to main

---
+++ helpers/checkout-feature-branch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+. ../shared/repos.sh
+
+cd ../../
+
+ORGANIZATION="DEFRA"
+
+ls
+
+BRANCH="${1:-develop}"
+
+for repository in "${repositories[@]}"
+do
+    cd $repository;
+    git checkout $BRANCH > /dev/null 2>&1
+    echo " Checking out: $(git branch --show-current) - $repository"
+    cd ..
+done
