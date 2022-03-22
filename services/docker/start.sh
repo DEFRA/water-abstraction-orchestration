3e3085b643c0a6698edeb6e455ae93a52d1bc70b
Jonathan Goulding <jonathangoulding21@gmail.com>
2022-03-22 14:19:06 +0000

    feat: login ghcr

--- services/docker/start.sh
+++ services/docker/start.sh
@@ -6,6 +6,8 @@ export REGISTRY="ghcr.io/defra"
 export DEFAULT_TAG="develop"
 export TAG="${1:-$DEFAULT_TAG}"
 
+echo "Format branch to - separated"
+TAG="${TAG//\//-}"
 echo "TAG = $TAG"
 
 GET_TAG() {
@@ -15,15 +17,18 @@ GET_TAG() {
 }
 
 export WATER_SERVICE_API_TAG=$(GET_TAG "water-abstraction-service")
-export COMPOSE_PROJECT_NAME="water-orchestration"
+export WATER_INTERNAL_UI_TAG=$(GET_TAG "water-abstraction-ui/internal-ui")
+export WATER_EXTERNAL_UI_TAG=$(GET_TAG "water-abstraction-ui/external-ui")
 
-export $(grep -v '^#' ../..//secrets/.env | xargs)
+
+export $(grep -v '^#' ../../secrets/.env | xargs)
 export $(grep -v '^#' ../variables.env | xargs)
 
 . ./variables.sh
 
 echo "HOST --- ${REDIS_HOST}"
 
+export COMPOSE_PROJECT_NAME="water-orchestration"
 docker-compose up -d
 
 # Branch: feat-pipeline-regression-tests
--- services/docker/variables.sh
+++ services/docker/variables.sh
@@ -3,6 +3,7 @@ export CRM_URI=http://host.docker.internal:8002/crm/1.0
 export RETURNS_URI=http://host.docker.internal:8006/returns/1.0
 export CHARGE_MODULE_ORIGIN=http://host.docker.internal:3005
 export EA_ADDRESS_FACADE_URI=http://host.docker.internal:9002
+export WATER_URI=http://host.docker.internal:8001/water/1.0
 export DATABASE_URL=postgres://water_user:password@host.docker.internal:5432/permits
 export REDIS_HOST=host.docker.internal
 # Missing from shared variables ?
